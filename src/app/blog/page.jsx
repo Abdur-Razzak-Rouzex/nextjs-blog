80e7ae8117f05a116298c2ae31dad3d77923ffa6
abdur-razzak-rouzex <abdurrazzak1663@gmail.com>
2024-01-31 22:14:05 +0600

    database connection added and getting the data from the database

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,10 +1,12 @@
 import PostCard from "@/components/postCard/postCard";
 import styles from "./blog.module.css";
-// import { getPosts } from "@/lib/data";
+import { getPosts } from "@/lib/data";
 
 // FETCH DATA WITH AN API
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/blog", {next:{revalidate:3600}});
+  const res = await fetch("http://localhost:3000/api/blog", {
+    next: { revalidate: 3600 },
+  });
 
   if (!res.ok) {
     throw new Error("Something went wrong");
@@ -14,12 +16,11 @@ const getData = async () => {
 };
 
 const BlogPage = async () => {
-
   // FETCH DATA WITH AN API
-  const posts = await getData();
+  // const posts = await getData();
 
   // FETCH DATA WITHOUT AN API
-  // const posts = await getPosts();
+  const posts = await getPosts();
 
   return (
     <div className={styles.container}>
